65469b43409b7579fc64eaf7ccf3af53cda86e4f
cx <cx@163.com>
2023-05-06 09:52:02 +0800

    cx hotfix first modify!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! cx hotfix first commit!");
+        System.out.println("Hello Git! cx hotfix first modify!");
     }
 
 }
