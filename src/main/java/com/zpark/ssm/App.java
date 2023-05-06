ffef525524ebacc63663e9c07ea91548ac75c593
cx <cx@163.com>
2023-05-06 10:03:33 +0800

    cx master second modify!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! cx hotfix first commit!");
         System.out.println("Hello Git! cx hotfix first modify!");
+        System.out.println("Hello Git! cx master second commit!");
     }
 
 }
